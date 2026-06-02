e0fd9dd922d1594034d7d18ace426ca38b7016af
Lucasjoaosantos <joaoluca778@gmail.com>
2026-06-02 09:50:51 -0300

    Update page.tsx

--- app/fichas/page.tsx
+++ app/fichas/page.tsx
@@ -7,9 +7,10 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from '@/components/ui/dialog'
-const { usuario, negado } = useGuard('estoque')
-if (negado) return null
+
 export default function FichasPage() {
+  const { usuario, negado } = useGuard('fichas')
+if (negado) return null
   const [fichas, setFichas] = useState<any[]>([])
   const [openModal, setOpenModal] = useState(false)
   const [clienteSelecionado, setClienteSelecionado] = useState<any>(null)
