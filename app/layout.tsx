36801b73d852e0f4dc17c9d53cf9ed924e879c31
Lucasjoaosantos <joaoluca778@gmail.com>
2026-06-02 13:57:53 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-'use client'
+import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessaoProvider } from '@/context/SessaoContext'
@@ -7,10 +7,16 @@ const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
+
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  subsets: ["latin",
+  subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  title: "Dom Black",
+  description: "Sistema de Gestão",
+};
 
 export default function RootLayout({
   children,
