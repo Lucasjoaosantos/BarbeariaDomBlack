c48766a688166267bdd79059b90bf984444d0d57
Lucasjoaosantos <joaoluca778@gmail.com>
2026-06-02 13:55:54 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,15 @@
 'use client'
-
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessaoProvider } from '@/context/SessaoContext'
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  subsets: ["latin"],
+  subsets: ["latin",
 });
 
 export default function RootLayout({
@@ -26,7 +23,9 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className={`${geistSans.className} font-sans min-h-full flex flex-col bg-zinc-950 text-white`}>
-        {children}
+        <SessaoProvider>
+          {children}
+        </SessaoProvider>
       </body>
     </html>
   );
